2457918643ba2ae1661c9f9d5a47c823e1840f1e
Aslambek Baimouradov <zandak@gmail.com>
2023-08-30 15:39:35 -0400

    db folder created

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const serverdb = require('./databases/dbpgrnr.js');
 
 app.use(express.static(path.join(__dirname, "./client/dist")));
 app.use(express.json());
