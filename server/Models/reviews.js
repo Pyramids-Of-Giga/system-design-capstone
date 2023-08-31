c96937f1baf2d4182e2994fb4f01743f8297a9cd
Aslambek Baimouradov <zandak@gmail.com>
2023-08-31 18:55:55 -0400

    first query

--- server/Models/reviews.js
+++ server/Models/reviews.js
@@ -1,2 +1,28 @@
-const client = require(/* FILL ME IN */);
-// utilize db connections here
+const Reviews = require("../../databases/dbpgrnr.js");
+
+exports.getReviews = (productId, sort, count, offset) => {
+  const query = `
+    SELECT * FROM rnr.reviews
+    WHERE product_id = $1
+    ORDER BY ${sort}
+    LIMIT $2
+    OFFSET $3;
+  `;
+
+  return Reviews.query(query, [productId, count, offset]);
+};
+
+
+// exports.addRecipe = (req, res) => {
+//   const formData = req.body;
+//   const newRecipe = new Recipe (formData);
+//   newRecipe.save()
+//     .then(newRecipe => {
+//       console.log('New recipe saved:', newRecipe);
+//       res.status(200).json({message: 'New recipe saved'});
+//     })
+//     .catch(error => {
+//       console.error('Error saving new recipe:', error);
+//       res.status(500).json({error: "Error saving new recipe"});
+//     });
+// };
