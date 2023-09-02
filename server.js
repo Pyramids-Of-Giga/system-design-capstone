d50658e602758fe97b6a4faad40e1c0ac859faee
Aslambek Baimouradov <zandak@gmail.com>
2023-09-02 14:29:43 -0400

    server redoing

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const reviewsRouter = require('./server/Routers/review_router.js');
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+// const reviewsRouter = require('./server/Routers/review_router.js');
 
 
-app.use(express.json());
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
-app.use('/reviews', reviewsRouter);
+// app.use(express.json());
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('Something went wrong!');
+// });
+// app.use('/reviews', reviewsRouter);
 
 
-app.get('/', (req, res) => {
-  res.send('Server is running')
-});
+// app.get('/', (req, res) => {
+//   res.send('Server is running')
+// });
 
-app.listen(PORT, () => {
-  console.log('Listening')
-});
+// app.listen(PORT, () => {
+//   console.log('Listening')
+// });
