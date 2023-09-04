3f909069c79e690ed701eba0451a08b66466f9f6
Aslambek Baimouradov <zandak@gmail.com>
2023-09-04 19:22:57 -0400

    optimized and passed my tests. Can do more here

--- db_test/rnr_testing.test.js
+++ db_test/rnr_testing.test.js
@@ -1,4 +1,6 @@
 
+//npm run test -- db_test/rnr_testing.test.js
+
 if (typeof TextEncoder === "undefined") {
   global.TextEncoder = require("util").TextEncoder;
 }
@@ -6,38 +8,28 @@ if (typeof TextDecoder === "undefined") {
   global.TextDecoder = require("util").TextDecoder;
 }
 
-
-const client = require('../databases/dbpgrnr.js');
-
-beforeAll(async () => {
-  await client.connect();
-});
-
-afterAll(async () => {
-  await client.end();
-});
+const pool = require('../databases/dbpgrnr.js');
 
 describe('Database Integration Tests', () => {
 
   test('reviews table should exist', async () => {
-    const result = await client.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'rnr' AND table_name = 'reviews')");
+    const result = await pool.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'rnr' AND table_name = 'reviews')");
     expect(result.rows[0].exists).toBe(true);
   });
 
   test('revpics table should exist', async () => {
-    const result = await client.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'rnr' AND table_name = 'revpics')");
+    const result = await pool.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'rnr' AND table_name = 'revpics')");
     expect(result.rows[0].exists).toBe(true);
   });
 
   test('chars table should exist', async () => {
-    const result = await client.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'rnr' AND table_name = 'chars')");
+    const result = await pool.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'rnr' AND table_name = 'chars')");
     expect(result.rows[0].exists).toBe(true);
   });
 
   test('revchars table should exist', async () => {
-    const result = await client.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'rnr' AND table_name = 'revchars')");
+    const result = await pool.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'rnr' AND table_name = 'revchars')");
     expect(result.rows[0].exists).toBe(true);
   });
 
 });
-
