0d20f229de77af4d51a410ec81b0ec056edeb068
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-06 10:15:41 -0400

    turned off products and reviews routes to avoid errors when I deploy qa

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const axios = require("axios");
-const handlers = require('./Models/reviews.js');
-const reviewsRouter = require('./Routers/review_router.js');
+// const handlers = require('./Models/reviews.js');
+// const reviewsRouter = require('./Routers/review_router.js');
 
-const serverdb = require('../databases/dbpgrnr.js');
-const productdb = require('../databases/products/products_db.js');
+// const serverdb = require('../databases/dbpgrnr.js');
+// const productdb = require('../databases/products/products_db.js');
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.json());
@@ -15,11 +15,11 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something went wrong!');
 });
 
-const productRouter = require("./Routers/product_router");
+// const productRouter = require("./Routers/product_router");
 const qaRouter = require("./Routers/qa_router");
 
-app.use("/products", productRouter);
-app.use("/reviews", reviewsRouter);
+// app.use("/products", productRouter);
+// app.use("/reviews", reviewsRouter);
 app.use("/qa", qaRouter);
 
 // added port into .env
@@ -29,44 +29,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening at port http://localhost:${port}`);
 });
-
-
-// old code from FEC
-// // routers
-// const QuestionAndAnswersRoutes = require("./server/Routers/qanda-routes");
-// const productRouter = require("./server/Routers/products-routes");
-// const reviewsRouter = require("./server/Routers/reviews-routes");
-// const cartRouter = require("./server/Routers/cart-routes");
-
-// // routes
-// app.use("/qa", QuestionAndAnswersRoutes);
-// app.use("/products", productRouter);
-// app.use("/reviews", reviewsRouter);
-// app.use("/cart", cartRouter);
-
-// chatgpt test
-// app.post("/customerservice", (req, res) => {
-//   const systemMessage = {
-//     role: "system",
-//     content:
-//       "Explain like an ecommerce customer service bot. Respond in only 1-2 sentences.",
-//   };
-//   const messages = [systemMessage, ...req.body];
-
-//   axios
-//     .post(
-//       "https://api.openai.com/v1/chat/completions",
-//       JSON.stringify({
-//         model: "gpt-3.5-turbo",
-//         messages: messages,
-//       }),
-//       {
-//         headers: {
-//           Authorization: `Bearer ${process.env.CHATGPT_API}`,
-//           "Content-Type": "application/json",
-//         },
-//       }
-//     )
-//     .then((results) => res.send(results.data))
-//     .catch((err) => console.log("fail", err));
-// });
